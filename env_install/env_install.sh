33c5e51cd8456da3536ae2ef5010ca0497747207
meh2280 <meh2280@columbia.edu>
2025-06-11 19:31:14 -0400

    Update submodule pytorch3d to new commit (add dynamically linked library for compatibility with 5090 workstation environment) and installation bash script (forgot to change)

--- env_install/env_install.sh
+++ env_install/env_install.sh
@@ -7,7 +7,7 @@ pip install trimesh
 pip install rtree 
 pip install pyrender
 
-conda install -y pytorch==2.4.0 torchvision==0.19.0 torchaudio==2.4.0 pytorch-cuda=12.1 -c pytorch -c nvidia
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu128
 pip install stannum
 pip install termcolor
 pip install fvcore
@@ -15,7 +15,6 @@ pip install wandb
 pip install moviepy imageio
 conda install -y opencv
 pip install cma
-pip install --no-index --no-cache-dir pytorch3d -f https://dl.fbaipublicfiles.com/pytorch3d/packaging/wheels/py310_cu121_pyt240/download.html
 
 # Install the env for realsense camera
 pip install Cython
@@ -24,24 +23,45 @@ pip install atomics
 pip install pynput
 
 # Install the env for grounded-sam-2
-pip install git+https://github.com/IDEA-Research/Grounded-SAM-2.git
-pip install git+https://github.com/IDEA-Research/GroundingDINO.git
+git clone https://github.com/IDEA-Research/Grounded-SAM-2.git
+cd Grounded-SAM-2/checkpoints/
+bash download_ckpts.sh
+cd ../gdino_checkpoints/
+bash download_ckpts.sh
+cd ../
+pip install -e .
+pip install --no-build-isolation -e grounding_dino
 
 # Install the env for image upscaler using SDXL
 pip install diffusers
 pip install accelerate
+
+pip install gsplat==1.4.0
+pip install kornia
+cd gaussian_splatting/submodules/diff-gaussian-rasterization/
+python setup.py build_ext --inplace
+pip install -e .
+cd ../simple-knn/
+pip install -e .
+cd ../../../
+
+pip install plyfile
+
+git clone https://github.com/facebookresearch/pytorch3d.git
+cd pytorch3d/
+pip install -e .
+cd ../
+
+pip install einops
 
 # Install the env for trellis
 cd data_process
 git clone --recurse-submodules https://github.com/microsoft/TRELLIS.git
 cd TRELLIS
 . ./setup.sh --basic --xformers --flash-attn --diffoctreerast --spconv --mipgaussian --kaolin --nvdiffrast
-
 cd ../..
 
-pip install gsplat==1.4.0
-pip install kornia
-cd gaussian_splatting/
-pip install submodules/diff-gaussian-rasterization/
-pip install submodules/simple-knn/
-cd ..
+wget https://github.com/Zarrac/flashattention-blackwell-wheels-whl-ONLY-5090-5080-5070-5060-flash-attention-/releases/download/FlashAttention/flash_attn-2.7.4.post1-rtx5090-torch2.7.0cu128cxx11abiTRUE-cp310-linux_x86_64.whl
+mv flash_attn-2.7.4.post1-rtx5090-torch2.7.0cu128cxx11abiTRUE-cp310-linux_x86_64.whl flash_attn-2.7.4.post1-0rtx5090torch270cu128cxx11abiTRUE-cp310-cp310-linux_x86_64.whl
+pip install flash_attn-2.7.4.post1-0rtx5090torch270cu128cxx11abiTRUE-cp310-cp310-linux_x86_64.whl
+rm flash_attn-2.7.4.post1-0rtx5090torch270cu128cxx11abiTRUE-cp310-cp310-linux_x86_64.whl
